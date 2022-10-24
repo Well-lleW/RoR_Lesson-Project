4f14894c9d14951949757371e0eeff26aa2d8c21
Well-lleW <91359668+Well-lleW@users.noreply.github.com>
2022-10-24 00:59:01 -0300

    rails g controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#landing_page"
 end
